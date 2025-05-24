8dcf056e6896698bd782d8a763cf8def9c490ee6
“Ankit <22amtics124@gmail.com>
2025-05-24 23:41:03 +0530

    add resume page with embedded Google Document

---
+++ app/Resume/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+    return (
+        <main>
+            <iframe width={"1000px"} height={"700px"} src="https://docs.google.com/document/d/e/2PACX-1vRqYffS_z-3rEkC8FXdPzXIExr8-vjA7o8ucwHq70mG032Hx0TileLo73xaRZO_BMuigRrwULcAb7hQ/pub?embedded=true"></iframe>
+        </main>
+    )
+}
