eca0ee7449ba5a1b769c1f0152e260218ba7c222
“Ankit <22amtics124@gmail.com>
2025-06-03 00:50:00 +0530

    update resume iframe source URL

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
             <iframe
   width="100%"
   height="700"
-  src="https://ik.imagekit.io/dqn1rnabh/Resume/Ankit%20Patil%20Resume%20.pdf?updatedAt=1748890344465"
+  src="https://media.varietyheaven.in/resume/Ankit Patil Resume .pdf"
   style={{ border: "1px solid black" }}
 ></iframe>
         </main>
